14a109819d392d9d538325662a052ebcb149c17a
robinmaa <robin@macbookpro-9c02.lampart.lan>
2022-10-05 14:16:17 +0200

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (  
    <Layout pageTitle="Startseite">
-        <p>Das ist die Startseite von Robin Maier</p>
+        <p>Das ist die Startseite von Lampart Holz</p>
+        <StaticImage
+        alt="Crestone Colorado"
+        src="../images/Crestone.jpg"
+        />
     </Layout>
   )
 }
